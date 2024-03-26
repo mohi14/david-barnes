887f7f6a1c1f209edb9a1ca2414e3dea4f7509a0
mohi14 <mohiuddinkhan1409@gmail.com>
2024-03-26 21:11:54 +0600

    update

--- src/components/Info/InfoBanner.jsx
+++ src/components/Info/InfoBanner.jsx
@@ -5,13 +5,13 @@ const InfoBanner = () => {
     return (
         <div className='info_banner'>
 
-            <p className='text-3xl lg:text-[108px] font-bold mb-5'>Hey, this is <br /> David</p>
+            <p className='text-3xl lg:text-[108px] font-bold mb-5 leading-[150%]'>Hey, this is <br /> David</p>
             <p className='text-[32px] font-normal mb-[83px] lg:ml-[104px] max-w-[920px]'>I’ve spent a couple of decades crafting brands and obsessing over
                 what makes business and project fly. I have a creative studio
                 called Charrette — collaborating on brave ideas with courageous
                 leaders.</p>
             <img className='mb-[82px]' src={banner} alt="" />
-            <p className='text-3xl lg:text-[84px] font-semibold mb-[64px] flex justify-center'>Here’s what you can <br /> expect from me...</p>
+            <p className='text-3xl lg:text-[84px] font-semibold mb-[64px] flex justify-center leading-[150%]'>Here’s what you can <br /> expect from me...</p>
         </div>
     );
 };
--- src/components/Info/InfoGrid.jsx
+++ src/components/Info/InfoGrid.jsx
@@ -4,12 +4,12 @@ const InfoGrid = () => {
     return (
         <div className='mb-[135px]'>
             <div className='grid grid-cols-1 lg:grid-cols-3 gap-[47px] mb-6'>
-                <div className='bg-black p-5 lg:px-5 lg:pt-[92px]  lg:pb-[111px]'>
-                    <p className='text-white text-2xl lg:text-[48px] font-bold'>I’ll bring years of experience, big energy and fresh thinking.</p>
+                <div className='bg-black p-5 lg:px-5 lg:pt-[92px]  lg:pb-[111px] w-full'>
+                    <p className='text-white text-2xl lg:text-[48px] font-bold leading-[150%]'>I’ll bring years of experience, big energy and fresh thinking.</p>
 
                 </div>
-                <div className='col-span-2 bg-black p-5 lg:px-5 lg:pt-[92px]  lg:pb-[111px]'>
-                    <p className='text-white text-2xl lg:text-[48px] font-bold'>Listen, challenge and reconnect the dots — joining your team with
+                <div className='lg:col-span-2 bg-black p-5 lg:px-5 lg:pt-[92px]  lg:pb-[111px]'>
+                    <p className='text-white text-2xl lg:text-[48px] font-bold leading-[150%]'>Listen, challenge and reconnect the dots — joining your team with
                         conviction and openness.</p>
 
 
@@ -18,13 +18,13 @@ const InfoGrid = () => {
             </div>
             <div className='grid grid-cols-1 lg:grid-cols-2 gap-5 '>
                 <div className='bg-black p-5 lg:px-5 lg:pt-[92px]  lg:pb-[111px]'>
-                    <p className='text-white text-2xl lg:text-[48px] font-bold'>Bridge the gap between creative and commercial objectives —
+                    <p className='text-white text-2xl lg:text-[48px] font-bold leading-[150%]'>Bridge the gap between creative and commercial objectives —
                         speaking both languages.</p>
 
 
                 </div>
                 <div className=' bg-black p-5 lg:px-5 lg:pt-[92px]  lg:pb-[111px]'>
-                    <p className='text-white text-2xl lg:text-[48px] font-bold'>Recognise potential — pushing brands to become better than you ever
+                    <p className='text-white text-2xl lg:text-[48px] font-bold leading-[150%]'>Recognise potential — pushing brands to become better than you ever
                         thought possible.</p>
 
 
